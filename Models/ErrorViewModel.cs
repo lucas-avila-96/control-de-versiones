b6412894c14c4a573f65d34e020f4e768d95ed65
Lucas <lucasavilaboca12@gmail.com>
2023-05-21 18:12:52 -0300

    Proyecto mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace control_de_versiones.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
